5962f12520d91e49bb8dd57da4d0d03443fd682d
栖童 <130018882+Linux-qitong@users.noreply.github.com>
2025-11-30 19:48:22 +0800

    fix: tags修复 useArticleIndex is not defined 错误

--- app/pages/tags.vue
+++ app/pages/tags.vue
@@ -9,7 +9,7 @@ const title = '标签'
 const description = `${appConfig.title}的所有文章标签。`
 useSeoMeta({ title, description })
 
-const { data: listRaw } = await useArticleIndex()
+const { data: listRaw } = await useAsyncData('index_posts', () => useArticleIndexOptions(), { default: () => [] })
 
 const articlesByTag = computed(() => {
 	const result: Record<string, any[]> = {}
